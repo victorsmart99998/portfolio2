6829d643d1162f43d8aefd7981b198566367b6b6
victorsmart <victchubxy@gmail.com>
2024-07-04 08:41:42 -0700

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,14 @@
 echo "BUILD START"
 
-python3.11 -m ensurepip
-python3.11 -m pip install -r requirements.txt
+python3.9 -m ensurepip
+python3.9 -m pip install -r requirements.txt
 
 echo "MAKE MIGRATIONS"
 
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-python3.11 manage.py collectstatics --noinput --clear
+python3.9 manage.py collectstatics --noinput --clear
 
 echo "BUILD END"
 
