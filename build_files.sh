4918965bd060f0e914f6b8b88035d0a33da7ce9b
victorsmart <victchubxy@gmail.com>
2024-07-04 09:29:23 -0700

    first commit

--- build_files.sh
+++ build_files.sh
@@ -8,7 +8,7 @@ echo "MAKE MIGRATIONS"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-python3.9 manage.py collectstatics --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
 
